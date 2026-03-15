931fa6dedbcaae58e8eae144e8541dcc7ae37eab
andreijeman <andrei.jeman3@gmail.com>
2026-03-15 18:24:37 +0200

    feat: add self host

--- src/Broker.Host/Program.cs
+++ src/Broker.Host/Program.cs
@@ -1,9 +1,52 @@
-﻿namespace Broker.Host
+﻿using Broker.Services.Services;
+using System;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace Broker.Host
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Task.Run(() => RunPublisherService());
+            Task.Run(() => RunSubscriberService());
+
+            Console.ReadKey();
+        }
+
+        static void RunPublisherService()
+        {
+
+            ServiceHost selfHost = new ServiceHost(typeof(PublisherService));
+
+            try
+            {
+                selfHost.Open();
+                Console.WriteLine("The publisher service is ready.");
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("An exception occurred: {0}", ce.Message);
+                selfHost.Abort();
+            }
+        }
+
+        static void RunSubscriberService()
+        {
+
+            ServiceHost selfHost = new ServiceHost(typeof(SubscriberService));
+
+            try
+            {
+                selfHost.Open();
+                Console.WriteLine("The subscriber service is ready.");
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("An exception occurred: {0}", ce.Message);
+                selfHost.Abort();
+            }
         }
     }
 }
